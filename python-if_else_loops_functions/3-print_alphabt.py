fbf32e18559d944a7c58885e1ade81eab7ab610c
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-21 03:11:46 -0700

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for n in range(97, 123):
-    if(n == 101) or (n == 113):
-        continue
-        print(chr(n).format(), end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print('{}'.format(chr(letter)), end='')
