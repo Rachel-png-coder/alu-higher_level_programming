deae7f2ade70f08b55de7016ce384c68a8bd0e7d
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-21 03:39:16 -0700

    2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for numbers in range(ord('a'), ord('z')+1:
-print('{}'.format(chr(numbers)), end='')
+for numbers in range(ord('a'), ord('z')  + 1:
+        print('{:c}'.format(numbers), end='')
