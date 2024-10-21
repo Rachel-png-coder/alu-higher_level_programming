ffc5ba09a1e2d166be383eaf70c7986507b0a675
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-20 18:52:02 -0700

    0-positive_or_negative.py

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+number = (-100, 100)
+print(number, end=" ")
+if number > 0:
+    print(is positive)
+elif number == 0:
+    print(is zero)
+else number < 0:
+    print(is negative)
