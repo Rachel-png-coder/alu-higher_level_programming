9129c51cf2212dc178f29f41ee14c804b33e17fb
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-21 02:51:42 -0700

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for ascii_code  in range(97, 123):
-    if(i == 101) or (i == 113):
+    if(ascii_code == 101) or (ascii_code == 113):
         continue
         print(chr(ascii_code).format(), end="")
