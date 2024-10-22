19f9c1571419a8b5801dd7c04f38380734b93e7f
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-22 08:15:53 -0700

    8-multiple_returns.py

--- python-import_modules/8-multiple_returns.py
+++ python-import_modules/8-multiple_returns.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-     len_sen = len(sentence)
-
-    if (len_sen == 0):
-        new_tuple = (len_sen, None)
-    else:
-        new_tuple = (len_sen, sentence[0])
-
-    return (new_tuple)
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a[:2] + (0, 0)
+    b = tuple_b[:2] + (0, 0)
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
