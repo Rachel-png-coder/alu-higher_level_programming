f5371e84ef5e9b47ba5a395022eed6647a195ba4
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-28 13:08:11 -0700

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     v = 0
     for v in range(x):
-    try:
-         print("{}".format(my_list[i]), end="")
-         v += 1
-    except IndexError:
-        break
+        try:
+            print("{}".format(my_list[v]), end="")
+        except IndexError:
+            break
+        v += 1
     print("")
     return (v)
