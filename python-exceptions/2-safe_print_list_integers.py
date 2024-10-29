912903e9cee3ec2479e7d87bbc7a7fcc847d1a32
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-28 22:10:45 -0700

    2-safe_print_list_integers.py

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    p = y = 0
+    while p < x:
+        try:
+            print("{:d}".format(my_list=[p]), end="")
+            y += 1
+        except (ValueError, TypeError):
+            continue
+        finally:
+            p += 1
+            print("")
+            return (Y)
