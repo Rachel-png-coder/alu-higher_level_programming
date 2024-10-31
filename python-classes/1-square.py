183813cce15e6ccbc9b5d4977873f4951cabc276
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-30 22:53:52 -0700

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    '''
+    define a square
+        has a private instance att: size
+    '''
+
+    def __init__(self, size):
+        ''' init size '''
+        self.__size = size
