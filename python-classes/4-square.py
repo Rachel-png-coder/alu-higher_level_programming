1624a32dfe5f7bede5e46eed9cc40ce12be6510e
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-31 00:01:49 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,30 +3,27 @@
 
 
 class Square:
-    '''
-    define a square
-        has a private instance att: size
-    '''
+    ''' class square that defones a square '''
 
     def __init__(self, size=0):
-        ''' init size '''
+        ''' Initialises a new square with a size '''
         self.__size = size
 
-        @property
-        def size(self):
-            ''' getter method '''
-            return self.__size
+    @property
+    def size(self):
+        ''' getter method '''
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            ''' setter method '''
+    @size.setter
+    def size(self, value):
+        ''' setter method '''
 
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValuError("size must be >= 0")
-            self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-            def area(self):
-                square_area = self.__size ** 2
-                return square_area
+    def area(self):
+        ''' Returns the area of a square '''
+        return(self.__size * self.__size)
