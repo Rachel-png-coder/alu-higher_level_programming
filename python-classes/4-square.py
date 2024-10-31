9e55cf94ec9646e06939c79e78ba8396ae9f0d15
Rachel-png-coder <r.toronga@alustudent.com>
2024-10-30 23:58:13 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,17 +14,18 @@ class Square:
 
         @property
         def size(self):
-            "returns the size att"
+            ''' getter method '''
             return self.__size
 
         @size.setter
-        def size(self, size):
-            '''asign the size to the size att'''
-            if not isinstance(size, int):
+        def size(self, value):
+            ''' setter method '''
+
+            if not isinstance(value, int):
                 raise TypeError("size must be an integer")
             elif size < 0:
                 raise ValuError("size must be >= 0")
-            self.__size = size
+            self.__size = value
 
             def area(self):
                 square_area = self.__size ** 2
