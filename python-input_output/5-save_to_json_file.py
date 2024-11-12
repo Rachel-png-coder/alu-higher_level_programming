4a1c099ccef3ed780b83c2c8d95ecf49f090f9b1
Rachel-png-coder <r.toronga@alustudent.com>
2024-11-11 23:37:12 -0800

    5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ a JSON representation:"""
+    """a JSON representation:"""
 
 
      with open(filename, 'w', encoding="utf-8") as f:
