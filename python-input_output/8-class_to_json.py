7b37d51a15bfdfbc1143089d0e9b75870e9a4a2a
Rachel-png-coder <r.toronga@alustudent.com>
2024-11-11 23:52:38 -0800

    8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a function that returns the dictionary description with simple data structure"""
+"""a function that returns the dictionary description with data structure"""
 
 
 def class_to_json(obj):
