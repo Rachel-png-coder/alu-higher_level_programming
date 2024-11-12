a0200e35eb6315934b56565791cae02aaab3e808
Rachel-png-coder <r.toronga@alustudent.com>
2024-11-11 23:50:42 -0800

    8-class_to_json.py

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that returns the dictionary description with simple data structure"""
+
+
+def class_to_json(obj):
+    """for JSON serialization of an object:"""
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
