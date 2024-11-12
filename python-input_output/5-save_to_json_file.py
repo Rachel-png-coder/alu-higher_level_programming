303d0cbe84e4ecc9dd27b8e436a3b75ed3389e75
Rachel-png-coder <r.toronga@alustudent.com>
2024-11-11 23:42:36 -0800

    5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a function that writes an Object to a text file, using a JSON representation"""
+"""a function that writes an Object to a text file,JSON representation"""
 import json
 
 
