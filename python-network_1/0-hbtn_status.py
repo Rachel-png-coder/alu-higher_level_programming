23c6da2cdac20aa0b1f77204b498c4dae5faa1f2
Rachel-png-coder <r.toronga@alustudent.com>
2024-11-23 23:05:06 -0800

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""fetches https://alx-intranet.hbtn.io/status"""
+
 import urllib.request
 
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+def main():
+    """Fetches url and displays the body of response"""
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        body = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    main()
