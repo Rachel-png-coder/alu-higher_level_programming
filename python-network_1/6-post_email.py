8b82654942b9398644a1f958aec364a8d68506f7
Rachel-png-coder <r.toronga@alustudent.com>
2024-11-25 02:13:59 -0800

    6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -7,10 +7,6 @@ import requests
 import sys
 
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
-    else:
-        print(response.text)
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print(r.text)
