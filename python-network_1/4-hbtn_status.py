187e68a7c9f59c12ae6ab659deee1931319c71bc
Rachel-png-coder <r.toronga@alustudent.com>
2024-11-25 01:44:18 -0800

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
-if __name__ == '__main__':
-    response = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
