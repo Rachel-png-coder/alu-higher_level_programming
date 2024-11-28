29ba733b030c6ea4f2e095094b2df74dbca4a175
Rachel-png-coder <r.toronga@alustudent.com>
2024-11-27 23:57:51 -0800

    7-model_state_fetch_all.py

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    This class is mapped to the 'states' table in the MySQL database.
+    Attributes:
+        __tablename__ (str): The name of the corresponding database table.
+        id (int): The primary key for the 'states' table.
+        name (str): The name of the state.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
