fe87e52ee0fb51938399da36e03097db1591212b
Rachel-png-coder <r.toronga@alustudent.com>
2025-01-23 23:44:05 -0800

    2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.length;
-switch (args) {
-	case 2:
-		console.log('No argument')
-		break;
-		case3 :
-		console.log('Argument found');
-		break;
-	default:
-		console.log('Arguments found')
-		break;
+if ((process.argv).length === 2) {
+  console.log('No argument');
+} else if ((process.argv).length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-
