f66892c71adfc6f511b2e58214188e432b96795a
Rachel-png-coder <r.toronga@alustudent.com>
2025-01-24 00:34:10 -0800

    3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args[0] == null) {
-   console.log('No argument');
+
+if (process.argv[2] === undefined) {
+  console.log('No argument');
 } else {
-  console.log(args[0]);
+  console.log(process.argv[2]);
 }
