51cf869a8866fe5d786cd12c48cfe57102f0c84b
Rachel-png-coder <r.toronga@alustudent.com>
2025-01-24 04:29:39 -0800

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
-const message = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(message[i]);
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < lang.length; i++) {
+  console.log(lang[i]);
 }
