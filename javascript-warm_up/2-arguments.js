9cc3a1df42761769b53596b9675075381ba96ad4
Rachel-png-coder <r.toronga@alustudent.com>
2025-01-23 23:29:34 -0800

    2-arguments.js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv.length;
+switch (args) {
+	case 2:
+		console.log('No argument')
+		break;
+		case3 :
+		console.log('Argument found');
+		break;
+	default:
+		console.log('Arguments found')
+		break;
+}
+
