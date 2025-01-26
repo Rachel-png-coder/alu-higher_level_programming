d9b7daad6cc17e5262400d5ab2f2fcef52c49e76
Rachel-png-coder <r.toronga@alustudent.com>
2025-01-26 15:44:27 -0800

    3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
+    if (w == null || h == null) return null;
+    if (w <= 0 || h <= 0) return null;
+    this.width = w;
+    this.height = h;
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let b = '';
-      for (let j = 0; j < this.width; j++) {
-        b += 'X';
-      }
-      console.log(b);
+      console.log('X'.repeat(this.width));
     }
   }
 }
-
+module.exports = Rectangle;
