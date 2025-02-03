11660db0c70cdecab735d7e759ff7775392e64fc
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-02 20:38:25 -0800

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// Writing a script that reads and prints the content of a file
 const fs = require('fs');
+const file = process.argv[2];
 
-fs.readFile(process.argv[2], (error, content) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(content.toString());
+fs.readFile(file, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
