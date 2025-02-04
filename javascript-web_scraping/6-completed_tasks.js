2095d0a075adef1669f0cd87f47a70048f4d6f87
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-03 19:25:16 -0800

    6-completed_tasks.js

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
+// A script that computes the number of tasks completed by user id
+const apiUrl = process.argv[2];
 const request = require('request');
 
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    const resp = {};
-    const json = JSON.parse(body);
-    for (let i = 0; i < json.length; i++) {
-      if (json[i].completed === true) {
-        if (resp[json[i].userId] === undefined) {
-          resp[json[i].userId] = 0;
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const result = {};
+    const parseData = JSON.parse(body);
+    parseData.forEach(item => {
+      if (item.completed) {
+        if (result[item.userId] === undefined) {
+          result[item.userId] = 1;
+        } else {
+          result[item.userId] += 1;
         }
-        resp[json[i].userId]++;
       }
-    }
-    console.log(resp);
+    });
+    console.log(result);
   }
 });
