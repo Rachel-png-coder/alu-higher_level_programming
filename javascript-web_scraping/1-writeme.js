2691d163788f7f60b87948355ba8256afd8fef26
Rachel-png-coder <r.toronga@alustudent.com>
2025-02-03 18:51:37 -0800

    1-writeme.js

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-const filePath = process.argv[2];
+const file = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
+fs.writeFile(file, content, err => {
   if (err) {
     console.error(err);
   }
